541e87044b7fcdbf40b562ace8f01a3ab38fc5df
Henrique Geroldo <henriquegeroldo@uol.com.br>
2024-04-26 20:59:01 -0300

    custom commands

--- cypress/e2e/3-loja-ebac/detalhes conta.cy.js
+++ cypress/e2e/3-loja-ebac/detalhes conta.cy.js
@@ -4,12 +4,15 @@ describe('Funcionalidade: detalhes da conta', () => {
   
   beforeEach(() => {
     cy.visit('minha-conta/edit-account')
-   cy.login('henrique.teste@teste.com.br' ,'Baranga1')
-
+    cy.fixture('perfil').then(login =>{
+      cy.login('henrique.teste@teste.com.br','Baranga1')
+    })
+   
   });
 
   it('Deve completar da conta com sucesso', () => {
-    
+    cy.detalhesConta('Henrique' ,'Geroldo', 'Geroldex')
+    cy.get('.woocommerce-message').should('contain' ,'Detalhes da conta modificados com sucesso.')
   });
 });
     
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -17,9 +17,10 @@ Cypress.Commands.add('preCadastro' , (email, senha , nome , sobrenome) => {
 
 })
 
-//Cypress.Commands.add('detalhesConta') ,(nome, sobrenome , usuario)
-//cy.get('#account_first_name').type(nome)
-//cy.get('#account_last_name').type(sobrenome)
-//cy.get('#account_display_name').type(usuario)
-//cy.get('.woocommerce-Button').click()
+Cypress.Commands.add('detalhesConta' ,(nome, sobrenome , usuario) =>{
+    cy.get('#account_first_name').type(nome)
+    cy.get('#account_last_name').type(sobrenome)
+    cy.get('#account_display_name').type(usuario)
+    cy.get('.woocommerce-Button').click()
 
+})
